9465f11e999d8fe56bdcdf25327bff01494620a6
kp1153 <kamta_tiwari@yahoo.com>
2026-03-09 12:34:57 +0530

    force deploy

--- app/medicines/[id]/edit/page.js
+++ app/medicines/[id]/edit/page.js
@@ -1,24 +1,19 @@
-import { db, initDB } from "@/lib/db";
+import { db } from "@/lib/db";
 import { medicines } from "@/lib/schema";
 import { eq } from "drizzle-orm";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import EditMedicineClient from "./EditMedicineClient";
 
-
 export default async function EditMedicinePage({ params }) {
-     await initDB();
   const cookieStore = await cookies();
   if (!cookieStore.get("auth")) redirect("/login");
-
   const { id } = await params;
   const result = await db
     .select()
     .from(medicines)
     .where(eq(medicines.id, parseInt(id)))
     .limit(1);
-
   if (!result[0]) redirect("/medicines");
-
   return <EditMedicineClient medicine={result[0]} />;
 }
