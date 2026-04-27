40ec0a35086297a9c4292cc8c74bef81e0734fd9
kp1153 <kamta_tiwari@yahoo.com>
2026-04-27 11:21:40 +0530

    force deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@libsql/client'],
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "medical-saas-32pt.vercel.app",
+          },
+        ],
+        destination: "https://clinic.nishantsoftwares.in/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
